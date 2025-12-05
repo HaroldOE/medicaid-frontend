796cd3ed1a08646ade3bc8408c5fb95a33a48a0e
HaroldOE <harold.ekempka@speedlinkng.com>
2025-12-06 00:36:52 +0100

    updated main

--- medicaid/src/App.jsx
+++ medicaid/src/App.jsx
@@ -3,26 +3,6 @@ import Router from "./router";
 function App() {
   return (
     <>
-      {/* <AppRouter /> */}
-      {/* <DoctorsDashboard /> */}
-      {/* <MessagePage /> */}
-      {/* <MyPatientsDashboard /> */}
-      {/* <PatientConsultationsPage /> */}
-      {/* <AppointmentsPage /> */}
-      {/* <PrescriptionsPage /> */}
-      {/* <AnalyticsPage /> */}
-      {/* <ProfilePage /> */}
-      {/* <DocumentationPage /> */}
-      {/* <PatientPrescriptionsPage /> */}
-      {/* <PatientConsultationPage /> */}
-      {/* <PatientMessagesPage /> */}
-      {/* <PatientMedicalHistoryPage /> */}
-      {/* <PatientAppointmentsPage /> */}
-      {/* <ProfileSettingsPage /> */}
-      {/* <PateintNotificationPage /> */}
-      {/* <PatientDocumentationPage /> */}
-      {/* <EmergecyContactCard /> */}
-      {/* <PatientMedHistory /> */}
       <Router />
     </>
   );
