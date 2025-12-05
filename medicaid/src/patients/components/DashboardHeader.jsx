21689514d11c896008fcb169696613c7aba37b1f
Ekemkpa Stephen <ekemkpas@gmail.com>
2025-12-05 07:49:12 +0100

    up dated

--- medicaid/src/patients/components/DashboardHeader.jsx
+++ medicaid/src/patients/components/DashboardHeader.jsx
@@ -1,8 +1,12 @@
 export default function DashboardHeader() {
   return (
     <div className="mb-10">
-      <h1 className="text-4xl font-bold text-gray-900">Welcome back, Patient</h1>
-      <p className="text-lg text-gray-600 mt-2">Manage your health effortlessly</p>
+      <h1 className="text-4xl font-bold text-gray-900">
+        Welcome back, Patient
+      </h1>
+      <p className="text-lg text-gray-600 mt-2">
+        Manage your health effortlessly
+      </p>
     </div>
   );
 }
