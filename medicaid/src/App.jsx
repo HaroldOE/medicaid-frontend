c5efc343ddc001b0a640a9e13179af64da8833c9
samueldm <samueldimkpa20@gmail.com>
2025-12-08 11:46:47 +0100

    update

---
+++ medicaid/src/App.jsx
@@ -0,0 +1,11 @@
+import Router from "./router";
+// import AppContext from "./contextapi/AppContext";
+function App() {
+  return (
+    <>
+      <Router />
+    </>
+  );
+}
+
+export default App;
