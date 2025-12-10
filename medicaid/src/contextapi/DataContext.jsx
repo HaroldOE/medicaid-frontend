6f630c7e9912f2bf07bb614c6353e0fed508610a
Engr Ekomobong Cletus <fynesticon@yahoo.com>
2025-12-10 04:39:13 +0100

    updated the env file

--- medicaid/src/contextapi/DataContext.jsx
+++ medicaid/src/contextapi/DataContext.jsx
@@ -5,7 +5,7 @@ import { useAuth } from "./AuthContext";
 const DataContext = createContext();
 export const useData = () => useContext(DataContext);
 
-const API = import.meta.env.VITE_API_URL || "http://localhost:3000";
+const API = import.meta.env.VITE_API_URL;
 
 export const DataProvider = ({ children }) => {
   const { user } = useAuth();
